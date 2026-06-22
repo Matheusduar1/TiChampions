c0c895c1bd7fb0a0903753ff33889fb84846e1cf
Matheus da Costa Duarte <matheus.duarte@gruporefrio.local>
2026-06-22 17:34:37 -0300

    v0.5

--- src/tichampions/TiChampionsMain.java
+++ src/tichampions/TiChampionsMain.java
@@ -4,18 +4,14 @@ import javax.swing.SwingUtilities;
 
 public class TiChampionsMain {
     public static void main(String[] args) {
-        // Boas práticas do Java Swing: Rodar na thread de interface
         SwingUtilities.invokeLater(() -> {
-            // Atualizamos o título da janela do jogo
             JFrame janela = new JFrame("TI Champions V5.0 - GUI Edition");
-            
             MotorGrafico motor = new MotorGrafico();
-            
             janela.add(motor);
-            janela.pack(); // Ajusta a janela pro tamanho do painel (1280x720)
+            janela.pack(); 
             janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            janela.setLocationRelativeTo(null); // Centraliza no monitor
-            janela.setResizable(false); // Por enquanto bloqueado, opções de resolução virão no SubMenu
+            janela.setLocationRelativeTo(null); 
+            janela.setResizable(true); 
             janela.setVisible(true);
         });
     }
